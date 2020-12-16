83757bad143b54af49c94db20926c5426fc404b5
Ayedux <aidanjw05@gmail.com>
2020-12-16 14:48:27 +1300

    Now checks if an individual num is a prime

--- PrimeChecker.py
+++ PrimeChecker.py
@@ -15,7 +15,7 @@ def primecheck(num):
         return True
     elif num % 2 == 0:
         return False
-    elif (num + 1) % 6 == 0 or (num -1) % 6 == 0:
+    elif (num + 1) % 6 == 0 or (num - 1) % 6 == 0:
         for i in range(3, maxtest):
             if num % i == 0:
                 return False
@@ -23,11 +23,16 @@ def primecheck(num):
     else:
         return False
 
-for n in range(1, number):
-    isprime = primecheck(n)
-    if isprime == True:
-        primes.append(n)
+#for n in range(1, number + 1):
+#    isprime = primecheck(n)
+#    if isprime == True:
+#        primes.append(n)
 
-print(primes)
+#print(primes)
+
+if primecheck(number):
+    print(f"{number} is a prime")
+else:
+    print(f"{number} is not a prime")
 
 print(f"Time take was {time.time()-start}")
